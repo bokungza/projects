6408a2d3ed664e0d38927e31dd632373a39f8a66
itsales singtaweesak <itsales.s@ku.th>
2019-10-31 16:39:40 +0700

    deleat edit product

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -60,9 +60,6 @@
 <a class="nav-link" href="/products/create">เพิ่มสินค้า</a>
 </li>
 <li class="nav-item">
-<a class="nav-link" href="#">แก้ไขสินค้า</a>
-</li>
-<li class="nav-item">
 <a class="nav-link" href="{{route('pays.index')}}">ดูการแจ้งชำระเงิน </a>
 </li>
 <li class="nav-item">
