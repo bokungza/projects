9525a1167178a7b201becb0deecb7524f57d350c
Sikharin Kadeeroj <sikharin.k@ku.th>
2019-11-02 19:10:57 +0700

    change login from email to username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
 
         ]);
@@ -43,5 +43,8 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function username(){
+      return 'username';
     }
 }
