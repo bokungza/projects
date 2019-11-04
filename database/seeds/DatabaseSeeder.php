942a8b02e6adf6ab080108431c4a23a08014ea0a
Sikharin Kadeeroj <sikharin.k@ku.th>
2019-11-04 23:54:39 +0700

    add OrdersTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(OrdersTableSeeder::class);
+
         //$this->call(PaysTableSeeder::class);
     }
 }
