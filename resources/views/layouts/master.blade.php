fad812af2afa253cc2c6bc224aaad12f6aa681f6
Sikharin Kadeeroj <sikharin.k@ku.th>
2019-11-26 14:13:47 +0700

    validate

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -38,21 +38,7 @@
     <script src="https://docraptor.com/docraptor-1.0.0.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
-  <!-- Popper JS -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-
-  <!-- Bootstrap js -->
-  <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-  <!-- Propeller textfield js -->
-  <script type="text/javascript" src="dist/js/propeller.min.js"></script>
-
-  <!-- Datepicker moment with locales -->
-  <script type="text/javascript" language="javascript" src="datetimepicker/js/moment-with-locales.js"></script>
-
-  <!-- Propeller Bootstrap datetimepicker -->
-  <script type="text/javascript" language="javascript" src="datetimepicker/js/bootstrap-datetimepicker.js"></script>
->
+  
     <script>
     // Change the type of input to password or text
         function Toggle() {
