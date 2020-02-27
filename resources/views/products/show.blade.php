18ede0998589ff1da670f8943a67f990a3d62907
6010450471 <pornnipa.k@ku.th>
2020-02-27 14:03:41 +0700

    fix bug

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -21,7 +21,7 @@
             @if (Gate::allows('add-cart',\App\Cart::class))
                 <form action="{{ route('cart.store') }}" method = 'post' style="float: left;width: 100%">
                     @csrf
-                    จำนวน : <input type="number" name="count" min="1" max="99" value="1" > กิโลกรัม
+                    จำนวน : <input type="number" name="count" min="1" max="{{$product->count}}" value="1" > กิโลกรัม
                     <input type="hidden" value="{{$product->id}}" name="pid">
                     <button class="btn btn-primary" type="submit" style="float: right" >Add to Cart</button>
                 </form>
